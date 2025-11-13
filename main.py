00cfed3575441146ff91b097274638728fe7e734
Dinyar <migranovdiniar@gmail.com>
2025-11-13 21:27:22 +0300

    ...

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr
 
 app = FastAPI()
 
@@ -9,11 +9,35 @@ def hello_index():
     "message": "Hello, World!"
     }
 
+
+@app.get("/hello/")
+def hello(name: str = "world"):
+    name = name.strip().title()
+    return {
+        "message": f"Hello, {name}!"
+    }
+
+
+@app.post("/users/")
+def create_user(user: EmailStr):
+    pass
+    
+
+
 @app.get("/items/")
 def list_items():
     return {
         "items": ["item1", "item2", "item3"]
     }
+
+
+@app.get("/items/latest/")
+def get_latest_item():
+    return {"item": {
+        "id": "0", 
+        "name": "The latest item"
+    }}
+
 
 @app.get("/items/{item_id}/")
 def get_item_by_id(item_id: str):
@@ -22,3 +46,5 @@ def get_item_by_id(item_id: str):
             "id": len(item_id),
         }
     }
+
+
