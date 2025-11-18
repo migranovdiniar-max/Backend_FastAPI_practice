572bf470f147f6fcca3cbc6a87bccc3e2bd9b4ce
Dinyar <migranovdiniar@gmail.com>
2025-11-18 17:08:36 +0300

    Cookie auth

--- api_v1/demo_auth/views.py
+++ api_v1/demo_auth/views.py
@@ -1,7 +1,9 @@
 import secrets
-from fastapi import APIRouter, Depends, HTTPException, status, Header
-from typing import Annotated
+import uuid
+from fastapi import APIRouter, Cookie, Depends, HTTPException, status, Header, Response
+from typing import Annotated, Any
 from fastapi.security import HTTPBasicCredentials, HTTPBasic
+from time import time
 
 router = APIRouter(prefix="/demo-auth", tags=["Demo Auth"])
 
@@ -83,4 +85,50 @@ def demo_auth_some_http_header(
     return {
         "message": f"Hello, {username}",
         "username": username
+    }
+
+
+COOKIES: dict[str, dict[str, Any]] = {}
+COOKIE_SESSION_ID_KEY = "web-app-session-id"
+
+
+def generate_session_id() -> str:
+    return uuid.uuid4().hex
+
+
+def get_session_data(
+        session_id: str = Cookie(alias=COOKIE_SESSION_ID_KEY),
+) -> dict:
+    if session_id not in COOKIES:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="not authenticated"
+        )
+    
+    return COOKIES[session_id]
+
+
+@router.post("/login-cookie/")
+def demo_auth_login_set_cookie(
+    response: Response, 
+    # auth_username: str = Depends(get_auth_user_username)
+    username: str = Depends(get_username_by_static_auth_token)
+):
+    session_id = generate_session_id()
+    COOKIES[session_id] = {
+        "username": username,
+        "login_at": int(time())
+    }
+    response.set_cookie(COOKIE_SESSION_ID_KEY, session_id)
+    return {"result": "ok"}
+
+
+@router.get("/check-cookie/")
+def demo_auth_check_cookie(
+    user_session_data: dict = Depends(get_session_data),
+):
+    username = user_session_data["username"]
+    return {
+        "message": f"Hello, {username}",
+        **user_session_data,
     }
